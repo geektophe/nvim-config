7379a71265df97a1ea981c9cab2f6f77b69f4def
Christophe Simon (Pro Nano) <christophe.simon@dailymotion.com>
2026-02-20 11:16:56 +0100

    Replaced nvim-tree by neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle explorer" },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true, -- Focus le fichier actuel
+      },
+      use_libuv_file_watcher = true, -- Auto-refresh
+    },
+  },
+}
